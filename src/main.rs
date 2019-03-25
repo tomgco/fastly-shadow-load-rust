343616bca250dc61680b5bb89b5dfaa9f48d69ff
Tom Gallacher <tomgallacher23@gmail.com>
2019-03-25 21:09:23 +0000

    Simple TCP server with JSON unmarshalling

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+extern crate tokio;
+extern crate tokio_codec;
+
+use tokio::codec::Decoder;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio_codec::LinesCodec;
+use serde_json::Value;
+
+use std::env;
+use std::net::SocketAddr;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>()?;
+
+    let socket = TcpListener::bind(&addr)?;
+    println!("Listening on: {}", addr);
+
+    let done = socket
+        .incoming()
+        .map_err(|e| println!("failed to accept socket; error = {:?}", e))
+        .for_each(move |socket| {
+            // Once we're inside this closure this represents an accepted client
+            // from our server. The `socket` is the client connection (similar to
+            // how the standard library operates).
+            //
+            // We're parsing each socket with the `LinesCodec` included in `tokio_io`,
+            // and then we `split` each codec into the reader/writer halves.
+            //
+            // See https://docs.rs/tokio-codec/0.1/src/tokio_codec/bytes_codec.rs.html
+            let framed = LinesCodec::new().framed(socket);
+            let (_writer, reader) = framed.split();
+
+            let processor = reader
+                .for_each(|line| {
+                    let v: Value = serde_json::from_str(&line)?;
+                    println!("v: {}", v["hello"]);
+                    Ok(())
+                })
+                // After our copy operation is complete we just print out some helpful
+                // information.
+                .and_then(|()| {
+                    println!("Socket received FIN packet and closed connection");
+                    Ok(())
+                })
+                .or_else(|err| {
+                    println!("Socket closed with error: {:?}", err);
+                    // We have to return the error to catch it in the next ``.then` call
+                    Err(err)
+                })
+                .then(|result| {
+                    println!("Socket closed with result: {:?}", result);
+                    Ok(())
+                });
+
+            // async by transfering ownership to tokio
+            tokio::spawn(processor)
+        });
+
+    tokio::run(done);
+    Ok(())
+}
