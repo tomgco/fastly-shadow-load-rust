5815b75da8263e21d6051136e700e1eaf21f2976
Tom Gallacher <tomgallacher23@gmail.com>
2019-03-26 23:37:03 +0000

    Arguments by ETOOMANY CLONES!
    A refactor here to have top-level closures should ease the need to use
    this many clones.
    
    But CBA and we can perf test to see how bad this is :]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate futures;
 extern crate tokio;
 extern crate tokio_codec;
 extern crate hyper;
+extern crate clap;
+
+use clap::{App, Arg};
 
 use tokio::codec::Decoder;
 use tokio::net::TcpListener;
@@ -12,7 +15,6 @@ use serde_json::Value;
 use hyper::{Body, Client, Request};
 use hyper::client::HttpConnector;
 
-use std::env;
 use std::net::SocketAddr;
 
 fn fetch_url(client: &Client<HttpConnector>, url: &str, host_override: String, method: &str) -> impl Future<Item=(), Error=()> {
@@ -42,16 +44,45 @@ fn fetch_url(client: &Client<HttpConnector>, url: &str, host_override: String, m
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
+    let matches = App::new("fastly-shadow-load")
+                        .version("1.0")
+                        .author("Tom Gallacher <me@tomg.co>")
+                        .about("Slam your servers with requests!")
+                        .arg(Arg::with_name("listen")
+                                    .short("l")
+                                    .long("listen")
+                                    .help("Address and port which the syslog server will bind to")
+                                    .default_value("127.0.0.1:8080"))
+                        .arg(Arg::with_name("times")
+                                    .short("x")
+                                    .long("times")
+                                    .help("Number of times to repeat a request")
+                                    .default_value("1"))
+                        .arg(Arg::with_name("target")
+                                    .short("t")
+                                    .long("target")
+                                    .help("Target HTTP host, where traffic will be sent, we use Kubernetes Services")
+                                    .default_value("http://my-service.default.svc.cluster.local"))
+                        .arg(Arg::with_name("host")
+                                    .long("host")
+                                    .default_value("other-service.default.svc.cluster.local")
+                                    .help("HTTP(s) host override, can be used to send traffic to ingress controllers"))
+                        .get_matches();
+
     let client = Client::new();
 
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = matches.value_of("listen").unwrap();
+    let times = matches.value_of("times").unwrap().parse::<i32>().unwrap();
+    let host_override = matches.value_of("host").unwrap().to_owned();
+
     let addr = addr.parse::<SocketAddr>()?;
-    let times = env::args().nth(2).unwrap_or("2".to_string()).parse::<i32>().unwrap();
 
     let socket = TcpListener::bind(&addr)?;
     println!("Listening on: {}", addr);
 
     let client = client.clone();
+    let host_override = host_override.clone();
+
     let done = socket
         .incoming()
         .map_err(|e| println!("failed to accept socket; error = {:?}", e))
@@ -60,6 +91,7 @@ fn main() -> Result<(), Box<std::error::Error>> {
             let (_writer, reader) = framed.split();
 
             let client = client.clone();
+            let host_override = host_override.clone();
             let processor = reader
                 .for_each(move |line| {
                     let v: Value = serde_json::from_str(&line)?;
@@ -68,11 +100,12 @@ fn main() -> Result<(), Box<std::error::Error>> {
                     let method = event["request"].as_str().unwrap();
 
                     for _i in 0..times {
+                        let host_override = host_override.clone();
                         tokio::spawn(
                             fetch_url(
                                 &client,
                                 url,
-                                "".to_string(),
+                                host_override,
                                 method
                             )
                         );
